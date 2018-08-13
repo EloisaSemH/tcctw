74bac9e7c9233d11e62de206643ffda27dc706a3
CaioAndrade160 <40966290+CaioAndrade160@users.noreply.github.com>
2018-08-13 11:07:30 -0300

    vai

--- pags/eventos.php
+++ pags/eventos.php
@@ -1,5 +1,5 @@
 <div class="card bg-dark text-white">
-  <img class="card-img" src="img/Logo.png" alt="Card image">
+  <img class="card-img" src="img/galeria/teste.jpg" alt="Card image">
   <div class="card-img-overlay">
     <h5 class="card-title">TÍTULO DA NOTÍCIAAAAAAAAAAAAA</h5>
     <p class="card-text">AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH</p>
