fd48f7c7fa25c6e05fd12618fa7a2c27e290a800
Caio Andrade <caio.andrad160@outlook.com>
2018-09-01 21:19:18 -0300

    eventos
    depois eu continuo

--- pags/eventos.php
+++ pags/eventos.php
@@ -1,8 +1,35 @@
-<div class="card bg-dark text-white">
-  <img class="card-img" src="img/galeria/teste.jpg" alt="Card image">
-  <div class="card-img-overlay">
-    <h5 class="card-title">TÍTULO DA NOTÍCIAAAAAAAAAAAAA</h5>
-    <p class="card-text">AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH</p>
-    <p class="card-text">OQ É ISSO?</p>
+<div class="container-fluid mx-auto" style="background: blue">
+  <div class="card w-50 bg-dark text-white float-left">
+    <img class="card-img" src="img/galeria/teste.jpg" alt="Card image" style="widht:100rem">
+    <div class="card-img-overlay">
+      <h5 class="card-title">TÍTULO DA NOTÍCIAAAAAAAAAAAAA</h5>
+      <p class="card-text">AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH</p>
+      <p class="card-text">OQ É ISSO?</p>
+    </div>
+  </div>
+
+  <div class="card w-50 bg-dark text-white mt-2">
+    <img class="card-img" src="img/galeria/teste.jpg" alt="Card image" style="widht:100rem">
+    <div class="card-img-overlay">
+      <h5 class="card-title">TÍTULO DA NOTÍCIAAAAAAAAAAAAA</h5>
+      <p class="card-text">AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH</p>
+      <p class="card-text">OQ É ISSO?</p>
+    </div>
+  </div>
+
+  <div class="card mt-2">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+      <a href="#" class="btn btn-primary">Button</a>
+    </div>
+  </div>
+
+  <div class="card mt-2">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+      <a href="#" class="btn btn-primary">Button</a>
+    </div>
   </div>
 </div>
