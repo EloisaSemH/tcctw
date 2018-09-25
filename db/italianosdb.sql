b09d78303091dd21f61ff74f95ec0c898556927f
Eloísa <eloisactrindade21@gmail.com>
2018-09-25 14:37:55 -0300

    banco de dados arquivo

---
+++ db/italianosdb.sql
@@ -0,0 +1,90 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: 25-Set-2018 às 19:37
+-- Versão do servidor: 10.1.13-MariaDB
+-- PHP Version: 7.0.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `italianosdb`
+--
+CREATE DATABASE IF NOT EXISTS `italianosdb` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `italianosdb`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `senha`
+--
+
+CREATE TABLE `senha` (
+  `usuario_us_cod` int(11) NOT NULL,
+  `se_senha` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `us_cod` int(11) NOT NULL,
+  `us_nome` varchar(100) NOT NULL,
+  `us_email` varchar(100) NOT NULL,
+  `us_sexo` varchar(1) NOT NULL,
+  `us_data` date NOT NULL,
+  `us_hora` time NOT NULL,
+  `us_ip` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `senha`
+--
+ALTER TABLE `senha`
+  ADD PRIMARY KEY (`usuario_us_cod`),
+  ADD KEY `fk_senha_usuario_idx` (`usuario_us_cod`);
+
+--
+-- Indexes for table `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`us_cod`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `us_cod` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Limitadores para a tabela `senha`
+--
+ALTER TABLE `senha`
+  ADD CONSTRAINT `fk_senha_usuario` FOREIGN KEY (`usuario_us_cod`) REFERENCES `usuario` (`us_cod`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
