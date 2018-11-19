e72213d4342cd5479472ae7f09b0a5093075cccd
Caio Andrade <caio.andrad160@outlook.com>
2018-11-19 19:16:21 -0200

    Nova Galeria

---
+++ pags/novagaleria.php
@@ -0,0 +1,116 @@
+<div class="conteiner my-3 ml-5 mr-5">
+<div class="card-columns">
+
+  <div class="card" data-toggle="modal" data-target="#1">
+    <img class="card-img-top" src="https://i.pinimg.com/originals/cd/09/27/cd0927946b6d4fb3597d0f50dab918b4.jpg" alt="Imagem de capa do card">
+  </div>
+  <div class="modal fade" id="1" tabindex="-1" role="dialog" aria-labelledby="1" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="1">Nome da foto</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <img class="img-fluid" src="https://i.pinimg.com/originals/cd/09/27/cd0927946b6d4fb3597d0f50dab918b4.jpg">
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+      </div>
+    </div>
+  </div>
+</div>
+  
+  <div class="card" data-toggle="modal" data-target="#2">
+    <img class="card-img-top" src="https://i1.wp.com/gamelogia.com.br/wp-content/uploads/2016/11/gamer.jpg?resize=1280%2C640&ssl=1" alt="Imagem de capa do card">
+  </div>
+  <div class="modal fade" id="2" tabindex="-1" role="dialog" aria-labelledby="2" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="2">Nome da foto</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <img class="img-fluid" src="https://i1.wp.com/gamelogia.com.br/wp-content/uploads/2016/11/gamer.jpg?resize=1280%2C640&ssl=1">
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+  <div class="card" data-toggle="modal" data-target="#3">
+    <img class="card-img-top" src="https://pm1.narvii.com/6522/b7db93c67c48a259c01240467d30584f010db6cd_hq.jpg" alt="Imagem de capa do card">
+  </div>
+  <div class="modal fade" id="3" tabindex="-1" role="dialog" aria-labelledby="3" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="3">Nome da foto</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <img class="img-fluid" src="https://pm1.narvii.com/6522/b7db93c67c48a259c01240467d30584f010db6cd_hq.jpg">
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="card" data-toggle="modal" data-target="#4">
+    <img class="card-img-top" src="http://www.rodoinside.com.br/wp-content/uploads/2015/12/sopro-do-dragao.jpg" alt="Imagem de capa do card">
+  </div>
+  <div class="modal fade" id="4" tabindex="-1" role="dialog" aria-labelledby="4" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="4">Nome da foto</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <img class="img-fluid" src="http://www.rodoinside.com.br/wp-content/uploads/2015/12/sopro-do-dragao.jpg">
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+  <div class="card" data-toggle="modal" data-target="#5">
+    <img class="card-img-top" src="http://www.topimagens.com.br/imagens/imagens-mais-novas.jpg" alt="Imagem do card">
+  </div>
+  <div class="modal fade" id="5" tabindex="-1" role="dialog" aria-labelledby="5" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="5">Nome da foto</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <img class="img-fluid" src="http://www.topimagens.com.br/imagens/imagens-mais-novas.jpg">
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+</div>
+</div>
